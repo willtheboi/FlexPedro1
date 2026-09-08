80a27a85d02b9ccd31605440d4e185ecfac49113
willtheboi <william.fleming@ums.legacypca.org>
2026-09-08 13:13:35 -0500

    tuning updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -17,10 +17,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .headingPIDFCoefficients(new PIDFCoefficients(15, 0.01, 0.4, 0))
-            .translationalPIDFCoefficients(new PIDFCoefficients(3,0,0.03,0))
-            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.003, 0.01, 0.001,0.6, 0.01))
-            .centripetalScaling(0.00001)
+            .headingPIDFCoefficients(new PIDFCoefficients(1, 0.01, 0.06, 0))
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.4,0,0.05,0))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.03, 0.001, 0.004,0.6, 0))
+            .centripetalScaling(0.0001)
             .forwardZeroPowerAcceleration(-30.062)
             .lateralZeroPowerAcceleration(-36.753)
             .mass(5);
